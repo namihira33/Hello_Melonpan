d05fcbfe2e9310af91107261339db721603a35bf
namihira33 <syuntakosu1011@gmail.com>
2019-12-16 12:25:03 +0900

    Change

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 
 const http = require('http');
-const hostname = '127.0.0.1';
-const port = 8000;
+//const hostname = '127.0.0.1';
+const port = process.env.PORT || 8000;
 var server = http.createServer();
 server.on('request', doRequest);
 // ファイルモジュールを読み込む
